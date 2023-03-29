f631414699efb944382a38e9de6ac4311c135723
berkayeser <berkay.eser@student.uantwerpen.be>
2023-03-29 21:28:19 +0200

    AST visualisatie

--- AST.py
+++ AST.py
@@ -4,75 +4,45 @@ import networkx as nx
 import networkx.drawing.nx_pydot as nxd
 import pydot
 import matplotlib.pyplot as plt
+import graphviz
 
 class AST:
     root = Node
 
     def vis(self):
-        G = pydot.Dot(graph_type="digraph", strict=True)
+        self.ast_to_dot(self.root)
 
+    # Define the ast_to_dot function
+    def ast_to_dot(self, node):
+        # Create a new Digraph object
+        dot = graphviz.Digraph()
 
-        nodes = [self.root]
-        id = 0
+        # Define a recursive function to visit each node in the AST
+        def visit(node, parent=None):
+            # Assign a unique ID to the node based on its memory address
+            id = str(hash(node))
+            # Label the node with its class name
+            label = node.getASTvalue()
+            dot.node(id, label)
 
-        while nodes:
-            t = 0
-            if nodes[0].type == "literal":
-                #print(nodes[0].value)
-                t = nodes[0].value
-            else:
-                t = nodes[0].type
-            u = pydot.Node(name=id, label=t)
-            id += 1
-            G.add_node(u)
-            #print(nodes[0].type + " en kids: " + str(len(nodes[0].children)))
-            for j in nodes[0].children:
+            # Add an edge from the parent node to this node
+            if parent:
+                dot.edge(parent, id)
 
-                if j.type == "literal":
-                    #print(j.value)
-                    t = j.value
-                else:
-                    t = j.type
+            # Recursively visit each child node
+            for child in node.children:
+                # Node child is another AST node, visit it
+                visit(child, id)
 
-                v = pydot.Node(name=id, label=t)
-                id += 1
+        # Call the visit function to traverse the entire AST
+        visit(node)
 
-                G.add_node(v)
-                G.add_edge(pydot.Edge(u,v))
-                nodes.append(j)
+        # Return the DOT code as a string
+        # return dot.source
 
-            nodes.pop(0)
-
-        G.write_png('ASTpng.png')
-
-    def vis2(self):
-        G = nx.DiGraph()
-        nodes = [self.root]
-
-        while nodes:
-            t = 00
-            if nodes[0].type == "literal":
-                print(nodes[0].value)
-                t = nodes[0].value
-            else:
-                t = nodes[0].type
-            for j in nodes[0].children:
-                G.add_edge(t, j.type)
-                nodes.append(j)
-            nodes.pop(0)
-
-        # nxd.write_dot(G, "ASTdot.dot")
-        # (graph,) = pydot.graph_from_dot_file('ASTdot.dot')
-        gr = nx.drawing.nx_pydot.to_pydot(G)
-        gr.write_png('ASTpng.png')
-
-
-        """u = pydot.Node(name="1", label="One")
-        v = pydot.Node(name="2", label="One")
-        r = pydot.Node(name="3", label="One")
-        G.add_node(u)
-        G.add_node(v)
-        G.add_node(r)
-        G.add_edge(pydot.Edge(u,v))
-        G.add_edge(pydot.Edge(u,r))"""
+        dotfilename = 'ast.dot'
+        with open(dotfilename, 'w') as f:
+            f.write(dot.source)
+        (graph,) = pydot.graph_from_dot_file(dotfilename)
+        graph.write_png('ASTpng.png')
 
--- main.py
+++ main.py
@@ -10,45 +10,7 @@ from AstOptimizer import AstOptimizer
 import pydot
 import networkx as nx
 import networkx.drawing.nx_pydot as nxd
-
-
 import graphviz
-# Define the ast_to_dot function
-# Define the ast_to_dot function
-def ast_to_dot(node):
-    # Create a new Digraph object
-    dot = graphviz.Digraph()
-
-    # Define a recursive function to visit each node in the AST
-    def visit(node, parent=None):
-        # Assign a unique ID to the node based on its memory address
-        id = str(hash(node))
-        # Label the node with its class name
-        label = node.getASTvalue()
-        dot.node(id, label)
-
-        # Add an edge from the parent node to this node
-        if parent:
-            dot.edge(parent, id)
-
-        # Recursively visit each child node
-        for child in node.children:
-            # Node child is another AST node, visit it
-            visit(child, id)
-
-    # Call the visit function to traverse the entire AST
-    visit(node)
-
-    # Return the DOT code as a string
-    return dot.source
-
-def writeDotCode(dot_c):
-    dotfilename = 'ast.dot'
-    with open(dotfilename, 'w') as f:
-        f.write(dot_c)
-    (graph,) = pydot.graph_from_dot_file(dotfilename)
-    graph.write_png('ASTpng.png')
-
 
 def main(argv):
     input_stream = FileStream("example.txt")
@@ -65,7 +27,7 @@ def main(argv):
     ast.root = visitor.visit(tree)
     ast = optimizer.constantFolding(ast)
 
-    writeDotCode(ast_to_dot(ast.root))
+    ast.vis()
 
 
 
