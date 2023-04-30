47b806c24c1f58161311f0c9e489938f6104a0c3
berkayeser <berkay.eser@student.uantwerpen.be>
2023-04-30 21:39:23 +0200

    Improved main function

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ from LLVMVisitor import LLVMVisitor
 # Just run "python3 main.py"
 
 def main(argv):
-    """if 'output' not in os.listdir(): # Als de folder 'output' nog niet bestaat
-        os.mkdir('output') # Maak een folder genaamd 'output' aan
+    visFlag = True
 
-    c_directory_path = "tests/text_files" """
+    if visFlag and 'output' not in os.listdir(): # Als de folder 'output' nog niet bestaat
+        os.mkdir('output') # Maak een folder genaamd 'output' aan
+        os.mkdir('output/dotfiles') # Maak een folder genaamd 'output' aan
+
+    #c_directory_path = "tests/text_files"
 
     ll_directory_path = "tests/ll_files"
-    tests_directory_path = "tests/projecten_123_zonder_main"
+    #tests_directory_path = "tests/projecten_123_zonder_main"
     tests_directory_path = "tests/projecten_123_zonder_main/p1"
 
-    #fid = 1
     for filename in os.listdir(tests_directory_path):
-
         file_path = os.path.join(tests_directory_path, filename)
-
         if os.path.isfile(file_path):
             input_stream = FileStream(file_path)
             lexer = CLexer(input_stream)
@@ -43,8 +43,8 @@ def main(argv):
             except Exception as error:
                  print(error, flush=True)
 
-            # ast.vis(fid)
-            # fid +=1
+            if visFlag:
+                ast.vis(filename)
 
         else:
             raise Exception(f"{file_path} not found.")
