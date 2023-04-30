9d920b5484120d8b28cf967d1969d3720565a72a
berkayeser <berkay.eser@student.uantwerpen.be>
2023-04-30 12:30:05 +0200

    Improved main function

--- main.py
+++ main.py
@@ -8,41 +8,46 @@ from LLVMVisitor import LLVMVisitor
 
 # Test script that automatically runs our Compiler on specified C files.
 # Just run "python3 main.py"
+
 def main(argv):
-    tests_directory_path = "tests/projecten_123_zonder_main/p2"
+    """if 'output' not in os.listdir(): # Als de folder 'output' nog niet bestaat
+        os.mkdir('output') # Maak een folder genaamd 'output' aan
+
+    c_directory_path = "tests/text_files" """
+
     ll_directory_path = "tests/ll_files"
-    fid = 1
+    tests_directory_path = "tests/projecten_123_zonder_main"
+    tests_directory_path = "tests/projecten_123_zonder_main/p1"
+
+    #fid = 1
     for filename in os.listdir(tests_directory_path):
+
         file_path = os.path.join(tests_directory_path, filename)
+
         if os.path.isfile(file_path):
             input_stream = FileStream(file_path)
             lexer = CLexer(input_stream)
             tokens = CommonTokenStream(lexer)
             parser = CParser(tokens)
             tree = parser.run()
+
             visitor = AstVisitor()
             optimizer = AstOptimizer()
-            # try:
-            #     print("Entering: " + file_path, flush=True)
-            #     ast = visitor.visit(tree)
-            #     llvm = LLVMVisitor()
-            #     llvm.file = ll_directory_path + "/" + filename[0:-2] + ".ll"
-            #     ast = optimizer.constantFolding(ast)
-            #     ast.generateLLVM(llvm)
-            # except Exception as error:
-            #     print(error, flush=True)
-            if filename == "proj2_man_pass_pointerReassignment.c":
-                print("Entering: " + file_path, flush=True)
-                ast = visitor.visit(tree)
-                llvm = LLVMVisitor()
-                llvm.file = ll_directory_path + "/" + filename[0:-2] + ".ll"
-                # if not file_path == "tests/c_files/test_1.txt":
-                ast = optimizer.constantFolding(ast)
-                ast.generateLLVM(llvm)
-                visitor.symbol_table.st_print()
+            try:
+                 print("Entering: " + filename, flush=True)
+                 ast = visitor.visit(tree)
+                 llvm = LLVMVisitor()
+                 llvm.file = ll_directory_path + "/" + filename[0:-2] + ".ll"
+                 ast = optimizer.constantFolding(ast)
+                 ast.generateLLVM(llvm)
+            except Exception as error:
+                 print(error, flush=True)
 
             # ast.vis(fid)
             # fid +=1
+
+        else:
+            raise Exception(f"{file_path} not found.")
 
 
 
